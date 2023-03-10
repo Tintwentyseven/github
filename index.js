bdb8c15e6609c9501a84a0462cac20232d8de6b1
Pham Trung Tin <phamtrungtinpy363@gmail.com>
2023-03-10 17:42:15 +0700

    RONALDO

--- index.js
+++ index.js
@@ -4,4 +4,5 @@ const magnifierEl = document.querySelector(".magnifier");
 
 magnifierEl.addEventListener("click", () => {
   searchBarContainerEl.classList.toggle("active");
+  const searchEl = document.querySelector(".magnifier-search");
 });
